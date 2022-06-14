d3fd4b1b8cc70d38d6c17ebead852f64e354a1c1
jmsalazardev <admin@jmsalazar.dev>
2022-06-14 10:38:30 +0200

    remove unused index

--- src/store/thunks/fetchAlbums.ts
+++ src/store/thunks/fetchAlbums.ts
@@ -1,4 +1,4 @@
-import { collection, query, getDocs, where, orderBy } from "firebase/firestore";
+import { collection, query, getDocs, where } from "firebase/firestore";
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { db } from '../../services/firebase';
 import { getCurrentUser } from '../../services/auth-service';
@@ -13,9 +13,10 @@ export const fetchAlbums = createAsyncThunk(
         const q = query(
             albumsRef,
             where("owner", "==", currentUser?.uid),
-            orderBy("title", "asc")
+            // orderBy("title", "asc")
         );
         const querySnapshot = await getDocs(q);
-        return querySnapshot.docs.map(fromFirestore);
+        return querySnapshot.docs.map(fromFirestore).sort((a, b) => (a.title > b.title ? 1 : b.title > a.title ? -1 : 0));
+        
     },
 );
